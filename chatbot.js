ca05d329d6c230c62d05280bce39439ff5746472
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-08-13 07:53:54 +0530

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const f = require("node-fetch");
+
+const client = new Discord.Client();
+
+client.on("message", async message => {
+// if (message.channel.name == "kuki-chats") {
+if (message.author.bot) return;
+if (message.content.includes(`what is your name`)) {
+return message.channel.send(`> ${message.content} \n Hello Otaku <@${message.author.id}>, My Name Is Serena`);
+ }
+if (message.content.includes(`What is your name`)) {
+return message.channel.send(`> ${message.content} \n Hello Otaku <@${message.author.id}>, My Name Is Serena`);
+ }
+  message.channel.startTyping();
+if (!message.content) return message.channel.send("Please say something.");
+f(`https://kuki.up.railway.app/Kuki/chatbot?message=${encodeURIComponent(message.content)}`)
+    .then(res => res.json())
+    .then(data => {
+        message.channel.send(`> ${message.content} \n  ${data.reply}`);
+    });
+      message.channel.stopTyping();
+}
+});
+
+client.login(process.env.TOKEN)
