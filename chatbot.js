6e8ca4b92001072f2f6136bf444aead84a587184
VEER2009 <89146739+VEER2009@users.noreply.github.com>
2021-08-23 12:34:39 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -4,7 +4,7 @@ const f = require("node-fetch");
 const client = new Discord.Client();
 
 client.on("message", async message => {
-if (message.channel.name == "rias chats") {
+if (message.channel.name == "┃°:busstop:┨rias-chats") {
 if (message.author.bot) return;
 if (message.content.includes(`what is your name`)) {
 return message.channel.send(`> ${message.content} \n Hello Otaku <@${message.author.id}>, My Name Is Serena`);
